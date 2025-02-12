f74f70ed7f551b01d49fed7f34524befed9de5b0
ryansingman <ryanjs@vt.edu>
2025-02-12 14:44:15 -0800

    Domain validation

--- infra/modules/domains/main.tf
+++ infra/modules/domains/main.tf
@@ -20,6 +20,14 @@ resource "aws_acm_certificate" "posthog-reverse-proxy" {
   }
 }
 
+resource "aws_route53_record" "posthog-reverse-proxy" {
+  zone_id = aws_route53_zone.posthog-reverse-proxy.zone_id
+  name = tolist(aws_acm_certificate.posthog-reverse-proxy.domain_validation_options)[0].resource_record_name
+  type = tolist(aws_acm_certificate.posthog-reverse-proxy.domain_validation_options)[0].resource_record_type
+  ttl = 300
+  records = [tolist(aws_acm_certificate.posthog-reverse-proxy.domain_validation_options)[0].resource_record_value]
+}
+
 resource "aws_route53_zone" "metronome-reverse-proxy" {
   name = "${var.metronome_reverse_proxy_subdomain}.${var.domain}"
   comment = "Metronome Reverse Proxy"
@@ -40,4 +48,12 @@ resource "aws_acm_certificate" "metronome-reverse-proxy" {
   lifecycle {
     create_before_destroy = true
   }
+}
+
+resource "aws_route53_record" "metronome-reverse-proxy" {
+  zone_id = aws_route53_zone.metronome-reverse-proxy.zone_id
+  name = tolist(aws_acm_certificate.metronome-reverse-proxy.domain_validation_options)[0].resource_record_name
+  type = tolist(aws_acm_certificate.metronome-reverse-proxy.domain_validation_options)[0].resource_record_type
+  ttl = 300
+  records = [tolist(aws_acm_certificate.metronome-reverse-proxy.domain_validation_options)[0].resource_record_value]
 }
