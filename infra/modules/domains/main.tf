93b944a4a61924bb29088f6f645192d0845b622e
ryansingman <ryanjs@vt.edu>
2025-02-12 14:33:22 -0800

    Create ACM certs

--- infra/modules/domains/main.tf
+++ infra/modules/domains/main.tf
@@ -7,11 +7,37 @@ resource "aws_route53_zone" "posthog-reverse-proxy" {
   }
 }
 
+resource "aws_acm_certificate" "posthog-reverse-proxy" {
+  domain_name = "${var.posthog_reverse_proxy_subdomain}.${var.domain}"
+  validation_method = "DNS"
+
+  tags = {
+    Environment = var.environment
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
 resource "aws_route53_zone" "metronome-reverse-proxy" {
   name = "${var.metronome_reverse_proxy_subdomain}.${var.domain}"
   comment = "Metronome Reverse Proxy"
 
   tags = {
     Environment = var.environment
+  }
+}
+
+resource "aws_acm_certificate" "metronome-reverse-proxy" {
+  domain_name = "${var.metronome_reverse_proxy_subdomain}.${var.domain}"
+  validation_method = "DNS"
+
+  tags = {
+    Environment = var.environment
+  }
+
+  lifecycle {
+    create_before_destroy = true
   }
 }
